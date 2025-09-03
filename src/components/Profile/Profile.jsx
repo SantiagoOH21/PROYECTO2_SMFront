4930b822dc62a3c25c4b37e666817582068ecda4
SantiagoOH21 <soh0621@gmail.com>
2025-09-03 16:48:40 +0200

    default avatar added

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,13 +2,17 @@ import { useSelector } from "react-redux";
 const Profile = () => {
   const { user } = useSelector((state) => state.auth);
 
+  const avatarUrl = user.avatar
+    ? `http://localhost:3000/uploads/${user.avatar}`
+    : "/default-avatar.png";
+
   return (
     <>
       <h1>Profile</h1>
       <p>{user.name}</p>
       <p>{user.email}</p>
       <img
-        src={`http://localhost:3000/uploads/${user.avatar}`}
+        src={avatarUrl}
         alt={user.name}
         style={{ width: "200px", height: "auto", borderRadius: "50%" }}
       />
